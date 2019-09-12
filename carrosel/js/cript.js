b219e7df85dc064ea4890be1e6b3137b28437272
Patrick <suportsystem.tech@gmail.com>
2019-09-11 21:28:06 -0300

    atualizando

--- carrosel/js/cript.js
+++ carrosel/js/cript.js
@@ -27,22 +27,22 @@ next.onclick = function(){
 
 back.onclick = function(){
     mysrc = myimg.getAttribute('src');
-    if(mysrc === './midia/beach-caribbean-coast-2598683.jpg'){
-        myimg.setAttribute('src', './midia/bahamas-beach-caribbean-2344530.jpg');
-    }
-    else if(mysrc === './midia/bahamas-beach-caribbean-2344530.jpg'){
-        myimg.setAttribute('src', './midia/background-backlit-beach-2305005.jpg');
+    if(mysrc === './midia/bahamas-beach-caribbean-2344530.jpg'){
+        myimg.setAttribute('src', './midia/beach-caribbean-coast-2598683.jpg' );
     }
     else if(mysrc === './midia/background-backlit-beach-2305005.jpg'){
-        myimg.setAttribute('src', "./midia/beach-blue-blue-sky-2612650.jpg");
+        myimg.setAttribute('src', './midia/bahamas-beach-caribbean-2344530.jpg');
     }
-    else if(mysrc === './midia/beach-blue-blue-sky-2612650.jpg'){
-        myimg.setAttribute('src', './midia/beach-caribbean-dawn-2598675.jpg');
+    else if(mysrc === "./midia/beach-blue-blue-sky-2612650.jpg"){
+        myimg.setAttribute('src', './midia/background-backlit-beach-2305005.jpg');
     }
     else if(mysrc === './midia/beach-caribbean-dawn-2598675.jpg'){
-        myimg.setAttribute('src', "./midia/balance-balancing-caribbean-sea-2419314.jpg");
+        myimg.setAttribute('src', './midia/beach-blue-blue-sky-2612650.jpg');
     }
     else if(mysrc === "./midia/balance-balancing-caribbean-sea-2419314.jpg"){
-        myimg.setAttribute('src', './midia/beach-caribbean-coast-2598683.jpg')
+        myimg.setAttribute('src', './midia/beach-caribbean-dawn-2598675.jpg');
+    }
+    else if(mysrc === './midia/beach-caribbean-coast-2598683.jpg'){
+        myimg.setAttribute('src', "./midia/balance-balancing-caribbean-sea-2419314.jpg");
     }
 }
